06a0ffae168d2cec4c8c27573d4a4861728cbd22
Slaymut <slimani.salim.pro@gmail.com>
2023-12-28 01:43:49 +0100

    Adding somes metadata

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -31,13 +31,18 @@ class WebScraper:
   def fetchMainElements(self, product_id):
     url = f"https://fr.aliexpress.com/item/{product_id}.html"
     title = self.fetchItem(url, 'h1', attrs={'data-pl': 'product-title'})
-    currentPrice = self.fetchItem(url, 'div', class_name="product-price-current")
+    
+    realPrice = self.fetchItem(url, 'span', class_name="price--originalText--Zsc6sMv")
+    discountedPrice = self.fetchItem(url, 'div', class_name="product-price-current")
+    discountPercentage = self.fetchItem(url, 'span', class_name="price--discount--xET8qnP")
+    likes = self.fetchItem(url, 'span', class_name="share-and-wish--wishText--g_o_zG7")
     
     print(f"Title: {title.text}")
-    print(f"Current Price: {currentPrice.text}")
+    print(f"Discount Price: {discountedPrice.text}")
+    print(f"Real Price: {realPrice.text}")
+    print(f"Discount Percentage: {discountPercentage.text}")
+    print(f"Likes: {likes.text}")
 
-# # Usage example
+# Usage example
 scraper = WebScraper('C:\\Users\\slaymut\\Documents\\Web Scraper Aliexpress\\aliexpress-webscraper\\chrome-driver\\chromedriver.exe')
-scraper.fetchMainElements("1005006132071310")
-
-
+scraper.fetchMainElements("1005006011508985")
