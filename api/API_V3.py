15b46b71c5cb21cf22526c09485f43a09bc59277
BulgogiBoy <baudouin.nguyen@hotmail.fr>
2024-01-10 20:36:01 +0100

    Ajout de l'arrêt du scrapper lorsqu'il y a moins de 60 items sur la page

--- api/API_V3.py
+++ api/API_V3.py
@@ -2,29 +2,18 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from selenium import webdriver
+from scraper.AliExpressItem import AliExpressItem
+from scraper.AliExpressStore import AliExpressStore
 from flask import Flask, request, jsonify
 from scraper.AliExpressNavigator import Navigator
 from scraper.AliExpressItemScraper import ItemScraper
 from pyspark.sql import SparkSession
-from flask_cors import CORS
 
 print(f"Chemin de recherche Python dans API_V3.py : {sys.path}")
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
-
-current_directory = os.getcwd()
-parent_directory = os.path.dirname(current_directory)
-chrome_driver_path = os.path.join(parent_directory, 'chrome-driver\\chromedriver.exe')
-
-driver = webdriver.Chrome(chrome_driver_path, options=options)
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello():
     return 'Bienvenue sur notre Projet BIG DATA !'
 
@@ -33,11 +22,25 @@ def hello():
 #     .appName("SparkHadoopAPI") \
 #     .getOrCreate()
 
+#Endpoint pour faire les Best-of Items
+@app.route('/best-of-items', methods=['GET'])
+def get_best_items():
+    return
+
+#Endpoint pour faire les Best-Of Stores
+@app.route('/best-of-store', methods=['GET'])
+def get_best_stores():
+    return
+
 # Endpoint pour effectuer une recherche sur AliExpress
 @app.route('/search', methods=['POST'])
 def search_on_aliexpress():
     try:
         # Récupérer les paramètres de la recherche depuis le corps de la demande
+        current_directory = os.getcwd()
+        parent_directory = os.path.dirname(current_directory)
+        chrome_driver_path = os.path.join(parent_directory, 'chrome-driver\\chromedriver.exe')
+        
         search_params = request.json
         search_filter = search_params.get('searchFilter', '')
         num_pages = search_params.get('numPages', 1)  # Nouveau paramètre pour le nombre de pages
@@ -49,7 +52,7 @@ def search_on_aliexpress():
         minimum = search_params.get('minimum', 0)
 
         # Initialiser l'objet Navigator
-        navigator = Navigator(driver)
+        navigator = Navigator(chrome_driver_path)
 
         # Charger les résultats de la recherche pour le nombre spécifié de pages
         items = []
@@ -64,9 +67,18 @@ def search_on_aliexpress():
                 maximum=maximum,
                 minimum=minimum
             )
+
+            # Arrêter le scraping si moins de 60 articles sont récupérés sur une page
+            if len(items) < 60:
+                message = 'Le scraping a été interrompu car moins de 60 articles ont été récupérés sur la page {}.'.format(page)
+                break
+
             items.extend(page_items)
 
-        return jsonify(items)
+        # Fermer le navigateur après avoir récupéré les résultats
+        navigator.driver.quit()
+
+        return jsonify({'message': message, 'items': items})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -76,6 +88,11 @@ def search_on_aliexpress():
 @app.route('/scrape_aliexpress_product', methods=['POST'])
 def scrape_aliexpress_product():
     try:
+        # Récupérer les paramètres de la recherche depuis le corps de la demande
+        current_directory = os.getcwd()
+        parent_directory = os.path.dirname(current_directory)
+        chrome_driver_path = os.path.join(parent_directory, 'chrome-driver\\chromedriver.exe')
+
         # Obtenez le produit ID à partir de la requête POST
         data = request.get_json()
         product_id = data.get('product_id')
@@ -85,19 +102,51 @@ def scrape_aliexpress_product():
             return jsonify({'error': 'Product ID is required'}), 400
 
         # Créez une instance du scraper
-        scraper_instance = ItemScraper(driver)
+        scraper_instance = ItemScraper(chrome_driver_path)
 
         # Appel à la fonction fetchAllData du scraper
         store, item = scraper_instance.fetchAllData(product_id)
-        scraper_instance.save_to_csv(
-            store=store,
-            item=item
-        )
         
         result = {
             'store': store,
             'item': item
         }
+        
+        aliexpressStore = AliExpressStore(
+          name=store['name'],
+          reviewPercentage=store['reviewPercentage'],
+          isChoiceStore=store['isChoiceStore'],
+          isPlusStore=store['isPlusStore'],
+          isGoldStore=store['isGoldStore'],
+          followers=store['followers'],
+          id=store['id'],
+          trustScore=store['trustScore'],
+          trustworthiness=store['trustworthiness']
+        )
+        
+        aliexpressItem = AliExpressItem(
+          id=item['id'],
+          title=item['title'],
+          price=item['price'],
+          valuePrice=item['valuePrice'] if 'valuePrice' in item else None,
+          shippingPrice=item['shippingPrice'] if 'shippingPrice' in item else None,
+          deliveryTime=item['deliveryTime'] if 'deliveryTime' in item else None,
+          deliveryDates=item['deliveryDates'] if 'deliveryDates' in item else None,
+          rating=item['rating'] if 'rating' in item else None,
+          reviewsNbr=item['reviewsNbr'] if 'reviewsNbr' in item else None,
+          sellsNbr=item['sellsNbr'] if 'sellsNbr' in item else None,
+          freeShippingAfter=item['freeShippingAfter'] if 'freeShippingAfter' in item else None,
+          trustScore=item['trustScore'],
+          trustworthiness=item['trustworthiness'],
+          isChoice=item['isChoice'],
+          isPlus=item['isPlus'],
+          store=aliexpressStore.id
+        )
+        
+        scraper_instance.save_to_csv(aliexpressItem, aliexpressStore)
+        
+        # Fermez le navigateur après avoir terminé le scraping
+        scraper_instance.driver.quit()
 
         if result:
             return jsonify(result), 200
